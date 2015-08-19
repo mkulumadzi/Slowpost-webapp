fa375bdbc126ab7aadc98e800d1478baa556fe7d
Evan Waters <evan.waters@gmail.com>
2015-08-19 16:12:12 -0700

    Added warning label that checks whether passwords match.

--- components/PasswordReset.js
+++ components/PasswordReset.js
@@ -2,12 +2,9 @@ var React = require('react');
 
 var PasswordReset = React.createClass({
 
-  // var username = "";
-  // var newPassword = "";
-  // var confirmPassword = "";
-
   getInitialState() {
     return {
+      warning: '',
       username: '',
       newPassword: '',
       confirmPassword: ''
@@ -15,14 +12,17 @@ var PasswordReset = React.createClass({
   },
 
   handleChangeForUsername(event) {
+    this.setState({warning: ''});
     this.setState({username: event.target.value});
   },
 
   handleChangeForNewPassword(event) {
+    this.setState({warning: ''});
     this.setState({newPassword: event.target.value});
   },
 
   handleChangeForConfirmPassword(event) {
+    this.setState({warning: ''});
     this.setState({confirmPassword: event.target.value});
   },
 
@@ -30,11 +30,17 @@ var PasswordReset = React.createClass({
 
     event.preventDefault();
 
-    // When the form is submitted, call the onPost callback that is passed to the component
-    this.props.onPost(this.state.username, this.state.newPassword);
-
     // Unfocus the text input field
     this.getDOMNode().querySelector('input').blur();
+
+    // When the form is submitted, call the onPost callback that is passed to the component
+
+    if(this.state.newPassword != this.state.confirmPassword){
+      this.setState({warning: "Passwords must match."});
+    }
+    else {
+      this.props.onPost(this.state.username, this.state.newPassword);
+    }
 
   },
 
@@ -45,14 +51,15 @@ var PasswordReset = React.createClass({
 				<div className="form-group">
 					<div className="col-xs-12 col-md-6 col-md-offset-3">
 						<div className="input-group">
+              <label id="warning-label">{this.state.warning}</label>
               <input type="text" className="form-control" id="username" placeholder="Enter username..."
               value={this.state.username} onChange={this.handleChangeForUsername} />
 							<input type="text" className="form-control" id="new-password" placeholder="Enter new password..."
 							value={this.state.newPassword} onChange={this.handleChangeForNewPassword} />
               <input type="text" className="form-control" id="confirm-password" placeholder="Confirm new password..."
               value={this.state.existingPassword} onChange={this.handleChangeForConfirmPassword} />
+              <button type="submit" id="submit-button" class="btn btn-default">Submit</button>
 						</div>
-            <button type="submit" class="btn btn-default">Submit</button>
 					</div>
 				</div>
 			</form>

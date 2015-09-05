b39e7e5193452559eb1437d0f604b83f5c0c0b05
Evan Waters <evan.waters@gmail.com>
2015-09-05 14:47:51 -0700

    Formatted legal section

--- components/Legal.js
+++ components/Legal.js
@@ -5,68 +5,68 @@ var Legal = React.createClass({
   render() {
 
     return (
-      <div>
+      <div className="document">
 
-      <h1>SLOWPOST PRIVACY POLICY</h1>
+        <h1>Slowpost Privacy Policy</h1>
 
-      <p>This privacy policy governs your use of the mobile software application Slowpost (Application) for mobile devices. The Application was created by Evan Waters and is a messaging application designed for creating meaningful, modern communication between friends and family.</p>
+        <p>This privacy policy governs your use of the mobile software application Slowpost (Application) for mobile devices. The Application was created by Evan Waters and is a messaging application designed for creating meaningful, modern communication between friends and family.</p>
 
-      <h2>WHAT INFORMATION DOES THE APPLICATION OBTAIN AND HOW IS IT USED?</h2>
+        <h2>What information does the application obtain, and how is it used?</h2>
 
-      <h3>USER PROVIDED INFORMATION</h3>
+        <h3>User Provided Information</h3>
 
-      <p>The Application obtains basic account information from you when you register, including your name, email address, phone number, and a username and password of your choice. Additional optional information, such as your home address, may be added to your Profile. You may optionally give the Application access to your phone address book, enabling the Application to search for registered users among your contact list.</p>
+        <p>The Application obtains basic account information from you when you register, including your name, email address, phone number, and a username and password of your choice. Additional optional information, such as your home address, may be added to your Profile. You may optionally give the Application access to your phone address book, enabling the Application to search for registered users among your contact list.</p>
 
-      <p>As you use the Application, it will also store the content you create, including the messages you write, the images you add, and associated metadata such as who the messages are to, and when they were created.</p>
+        <p>As you use the Application, it will also store the content you create, including the messages you write, the images you add, and associated metadata such as who the messages are to, and when they were created.</p>
 
-      <p>We may also use the information you provided us to contact you and provide you with important information, including marketing promotions and required notices.</p>
+        <p>We may also use the information you provided us to contact you and provide you with important information, including marketing promotions and required notices.</p>
 
-      <h3>AUTOMATICALLY COLLECTED INFORMATION</h3>
+        <h3>Automatically Collected Information</h3>
 
-      <p>In addition, the Application may collect certain information automatically, including, but not limited to, the type of mobile device you use, your mobile devices unique device ID, the IP address of your mobile device, your mobile operating system, and information about the way you use the Application.</p>
+        <p>In addition, the Application may collect certain information automatically, including, but not limited to, the type of mobile device you use, your mobile devices unique device ID, the IP address of your mobile device, your mobile operating system, and information about the way you use the Application.</p>
 
-      <h2>DOES THE APPLICATION COLLECT PRECISE REAL TIME LOCATION INFORMATION OF THE DEVICE?</h2>
+        <h2>Does the application collect realtime, precise location information about the device?</h2>
 
-      <p>This Application does not collect precise information about the location of your mobile device.</p>
+        <p>This Application does not collect precise information about the location of your mobile device.</p>
 
-      <h2>DO THIRD PARTIES SEE AND/OR HAVE ACCESS TO INFORMATION OBTAINED BY THE APPLICATION?</h2>
+        <h2>Do third parties see and/or have access to information obtained by the Application?</h2>
 
-      <p>The Application may submit aggregated, anonymized data to external services to help us improve the Application and the services we offer you. We will share your information with third parties only in the ways that are described in this privacy statement.</p>
+        <p>The Application may submit aggregated, anonymized data to external services to help us improve the Application and the services we offer you. We will share your information with third parties only in the ways that are described in this privacy statement.</p>
 
-      <p>We may disclose User Provided and Automatically Collected Information:</p>
+        <p>We may disclose User Provided and Automatically Collected Information:</p>
 
-      <li>as required by law, such as to comply with a subpoena, or similar legal process;</li>
-      <li>when we believe in good faith that disclosure is necessary to protect our rights, protect your safety or the safety of others, investigate fraud, or respond to a government request;</li>
-      <li>with our trusted services providers who work on our behalf, do not have an independent use of the information we disclose to them, and have agreed to adhere to the rules set forth in this privacy statement.</li>
-      <li>if the Application and its parent company are involved in a merger, acquisition, or sale of all or a portion of its assets, you will be notified via email and/or a prominent notice on our Web site of any change in ownership or uses of this information, as well as any choices you may have regarding this information.</li>
+        <li>as required by law, such as to comply with a subpoena, or similar legal process;</li>
+        <li>when we believe in good faith that disclosure is necessary to protect our rights, protect your safety or the safety of others, investigate fraud, or respond to a government request;</li>
+        <li>with our trusted services providers who work on our behalf, do not have an independent use of the information we disclose to them, and have agreed to adhere to the rules set forth in this privacy statement.</li>
+        <li>if the Application and its parent company are involved in a merger, acquisition, or sale of all or a portion of its assets, you will be notified via email and/or a prominent notice on our Web site of any change in ownership or uses of this information, as well as any choices you may have regarding this information.</li>
 
-      <h2>WHAT ARE MY OPT-OUT RIGHTS?</h2>
+        <h2>What are my opt-out rights?</h2>
 
-      <p>You can stop all collection of information by the Application easily by uninstalling the Application. You may use the standard uninstall processes as may be available as part of your mobile device or via the mobile application marketplace or network. You can also request to opt-out via email, at support@slowpost.me.</p>
+        <p>You can stop all collection of information by the Application easily by uninstalling the Application. You may use the standard uninstall processes as may be available as part of your mobile device or via the mobile application marketplace or network. You can also request to opt-out via email, at <a className="link">support@slowpost.me</a>.</p>
 
-      <h2>DATA RETENTION POLICY</h2>
+        <h2>Data Retention Policy</h2>
 
-      <p>We will retain User Provided data for as long as you use the Application and for a reasonable time thereafter. We will retain Automatically Collected information for up to 24 months and thereafter may store it in aggregate. If you’d like us to delete User Provided Data that you have provided via the Application, please contact us at support@slowpost.me. Please note that some or all of the User Provided Data may be required in order for the Application to function properly.</p>
+        <p>We will retain User Provided data for as long as you use the Application and for a reasonable time thereafter. We will retain Automatically Collected information for up to 24 months and thereafter may store it in aggregate. If you’d like us to delete User Provided Data that you have provided via the Application, please contact us at <a className="link">support@slowpost.me</a>. Please note that some or all of the User Provided Data may be required in order for the Application to function properly.</p>
 
-      <h2>CHILDREN</h2>
+        <h2>Children</h2>
 
-      <p>We do not use the Application to knowingly solicit data from or market to children under the age of 13. If a parent or guardian becomes aware that his or her child has provided us with information without their consent, he or she should contact us at support@slowpost.me. We will delete such information as requested by parents or guardians.</p>
+        <p>We do not use the Application to knowingly solicit data from or market to children under the age of 13. If a parent or guardian becomes aware that his or her child has provided us with information without their consent, he or she should contact us at <a className="link">support@slowpost.me</a>. We will delete such information as requested by parents or guardians.</p>
 
-      <h2>SECURITY</h2>
+        <h2>Security</h2>
 
-      <p>We are concerned about safeguarding the confidentiality of your information and provide physical, electronic, and procedural safeguards to protect information we process and maintain. User Provided data and Automatically Collected information are securely stored in the cloud via a third-party provider (Heroku). We limit access to this data to authorized employees and contractors. Please be aware that, although we endeavor provide reasonable security for information we process and maintain, no security system can prevent all potential security breaches.</p>
+        <p>We are concerned about safeguarding the confidentiality of your information and provide physical, electronic, and procedural safeguards to protect information we process and maintain. User Provided data and Automatically Collected information are securely stored in the cloud via a third-party provider (Heroku). We limit access to this data to authorized employees and contractors. Please be aware that, although we endeavor provide reasonable security for information we process and maintain, no security system can prevent all potential security breaches.</p>
 
-      <h2>CHANGES</h2>
+        <h2>Changes</h2>
 
-      <p>This Privacy Policy may be updated from time to time for any reason. We will notify you of any changes to our Privacy Policy by posting the new Privacy Policy here and informing you via email or text message. You are advised to consult this Privacy Policy regularly for any changes, as continued use is deemed approval of all changes.</p>
+        <p>This Privacy Policy may be updated from time to time for any reason. We will notify you of any changes to our Privacy Policy by posting the new Privacy Policy here and informing you via email or text message. You are advised to consult this Privacy Policy regularly for any changes, as continued use is deemed approval of all changes.</p>
 
-      <h2>YOUR CONSENT</h2>
+        <h2>Your consent</h2>
 
-      <p>By using the Application, you are consenting to our collecting, storing, deleting, using, combining and disclosing your information as set forth in this Privacy Policy now and as amended by us.</p>
+        <p>By using the Application, you are consenting to our collecting, storing, deleting, using, combining and disclosing your information as set forth in this Privacy Policy now and as amended by us.</p>
 
-      <h2>CONTACT US</h2>
+        <h2>Contact Us</h2>
 
-      <p>If you have any questions regarding privacy while using the Application, or have questions about our practices, please contact us via email at support@slowpost.me.</p>
+        <p>If you have any questions regarding privacy while using the Application, or have questions about our practices, please contact us via email at <a className="link">support@slowpost.me</a>.</p>
 
       </div>
     );

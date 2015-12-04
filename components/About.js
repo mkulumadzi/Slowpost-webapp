2a22256d89f2f584f9e45aa4e737de6eb1f9e5c4
Evan Waters <evan.waters@gmail.com>
2015-12-04 12:53:59 -0800

    Added FB pixel and new section with an example of a card.

--- components/About.js
+++ components/About.js
@@ -73,6 +73,20 @@ var About = React.createClass({
             </div>
           </div>
         </div>
+        <div className="row grey">
+          <div className="card centered col-xs-10 col-xs-offset-1 col-sm-8 col-sm-offset-2 col-md-8 col-md-offset-2">
+            <div className="container-fluid">
+              <div className="row">
+                <div className="col-xs-12 col-sm-6 col-md-6">
+                  <img className="app-image" src="assets/turtle-youre-great.jpeg" alt="How Slopost works"/>
+                </div>
+                <div className="col-xs-12 col-sm-6 col-md-6">
+                  <p className="text-box">Check out our gallery often - we keep it updated to help you say more!</p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
         <div className="container centered">
           <h1>Available for free on iOS!</h1>
           <a href="https://ad.apps.fm/gIJxomrWUEg2Ffgzw8O2AfE7og6fuV2oOMeOQdRqrE27j-ERwjcsNO7J1qdLzXLo7nkyfM0fYq1OahDQ-WIbQoI3KrEfrKcPhcwwOVMS9d6zo9Hnp7Dp_hgoVh9PhhZv"><img className="download-icon" src="assets/Download_on_the_App_Store_Badge_US-UK_135x40.svg" alt="Download on the App Store"/></a>
